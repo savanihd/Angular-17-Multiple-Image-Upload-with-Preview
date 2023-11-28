840cc1ed233107d51d5447e61866fa1a494bcac9
Hardik Savani <itsolutionstuff@gmail.com>
2023-11-28 18:48:17 +0530

    added angular 17 multi image upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import { FileUploadComponent } from './file-upload/file-upload.component';
+import { ImageUploadComponent } from './image-upload/image-upload.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FileUploadComponent],
+  imports: [CommonModule, RouterOutlet, ImageUploadComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/image-upload/image-upload.component.ts
+++ src/app/image-upload/image-upload.component.ts
@@ -15,6 +15,8 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 })
 export class ImageUploadComponent {
 
+  images : any = [];
+  
   /*------------------------------------------
   --------------------------------------------
   Declare Form
@@ -25,14 +27,14 @@ export class ImageUploadComponent {
     file: new FormControl('', [Validators.required]),
     fileSource: new FormControl('', [Validators.required])
   });
-  
+    
   /*------------------------------------------
   --------------------------------------------
   Created constructor
   --------------------------------------------
   --------------------------------------------*/
   constructor(private http: HttpClient) { }
-  
+    
   /**
    * Write code on Method
    *
@@ -41,37 +43,40 @@ export class ImageUploadComponent {
   get f(){
     return this.myForm.controls;
   }
-  
+    
   /**
    * Write code on Method
    *
    * @return response()
    */
   onFileChange(event:any) {
-  
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.myForm.patchValue({
-        fileSource: file
-      });
+    if (event.target.files && event.target.files[0]) {
+        var filesAmount = event.target.files.length;
+        for (let i = 0; i < filesAmount; i++) {
+                var reader = new FileReader();
+     
+                reader.onload = (event:any) => {
+                  console.log(event.target.result);
+                   this.images.push(event.target.result); 
+   
+                   this.myForm.patchValue({
+                      fileSource: this.images
+                   });
+                }
+    
+                reader.readAsDataURL(event.target.files[i]);
+        }
     }
-  } 
-  
+  }
+    
   /**
    * Write code on Method
    *
    * @return response()
    */
   submit(){
-    const formData = new FormData();
-
-    const fileSourceValue = this.myForm.get('fileSource')?.value;
-
-    if (fileSourceValue !== null && fileSourceValue !== undefined) {
-        formData.append('file', fileSourceValue);
-    }
-     
-    this.http.post('http://localhost:8001/upload.php', formData)
+    console.log(this.myForm.value);
+    this.http.post('http://localhost:8001/upload.php', this.myForm.value)
       .subscribe(res => {
         console.log(res);
         alert('Uploaded Successfully.');
